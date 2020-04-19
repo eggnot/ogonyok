5331191bd04643a51210da178a6d80aef1929dd0
jora <el.mexan@gmail.com>
2020-04-19 18:27:22 +0700

    промежуточная, балан изменен, ритм отключен

--- ogonyok-godot/Calc_and_rithm.gd
+++ ogonyok-godot/Calc_and_rithm.gd
@@ -1,24 +1,36 @@
 extends Node2D
 
-var t = 0
-var fase = 0
-var fase_trig = 0.5	# порог срабатывания кнопки
+# Сложность попадание в ритм. 0 = отключено, 0.8 - сложно
+var fase_trig = 0.0
+# Проверять "если за такт не нажата кнопка"
+var auto_fuckup = false
+
+# Технические переменные для задачи ритма
+var t = 0;	var fase = 0
 var k_fase = 100		# длина цикла в кадрах
 var pl_moved = false	# проверка на нажатие кнопки
 
-var auto_fuckup = false
 
 var d = {}
 
 func reset():
 	d = {
+		# Начальные условия игры
 		fire = 80, wood = 50, people = 100, turn = 0,
 		
-		k01 = 2.5, k02 = 3, k03 = .02, k04 = .02,
-		k11 =5, k12 = .06,
-		k21 = 70, k22 = 5,
-		k31 = 1,
-		k41 = 3, k42 = 10,
+		k01 = 15,  # Потухание костра за ход
+		k02 = 2.5, 	# Разгорание костра за каждое полено
+		
+		k11 = 4, 	# Подкидывание лично шаманом
+		k12 = 0.03,	# Бонус подброса дров за человека
+		k13 = 0.03,	# Бонус подброса да количество дров
+		k14 = 0.25,	# Cбор дров с человека
+		
+		k21 = 50, 	# Безопасный уровень огня
+		k22 = 15, 	# Урон от прихода волков
+		
+		k41 = 0, 	# Естественная убыль храбрости
+		k42 = 10,	# Прибыль храбрости от танца
 		
 		x=0, y=0, z=0
 	}
@@ -40,9 +52,9 @@ func _process(delta):
 			_on_fuckup_pressed()
 	
 	#t = OS.get_time()['second']	# Жестко тормозит
-	fase = cos(2*3.14*t/k_fase)
+	fase = (1+cos(2*3.14*t/k_fase))/2
 	
-	$Control/time_label.text = str(fase)
+	#$Control/time_label.text = str(fase)
 	
 	$Control/gather.rect_scale.x = 1+fase/10
 	$Control/gather.rect_scale.y = 1+fase/10
@@ -59,7 +71,12 @@ func _process(delta):
 #	pass
 
 func calc():
-	d.fire += -d.k01 + d.k02*d.x + d.k03*d.x*d.wood + d.k04*d.x*d.people
+	var drop = 0
+	drop = d.k11 + d.k12 * d.people + d.k13 * d.wood
+	if drop > d.wood:
+		drop = d.wood
+	
+	d.fire = d.fire - d.k01 + d.k02 * d.x * drop
 	
 	if (d.fire>100): d.fire=100
 	
@@ -67,20 +84,22 @@ func calc():
 		d.fire=0
 		d.people = 0
 	
-	d.wood = d.wood - d.k11*d.x + d.k12*d.people*d.z
-	
+	d.wood = d.wood - drop * d.x + d.k14 * d.people * d.z
 	if (d.wood<0): d.wood=0
+	if (d.wood>100): d.wood=100
 	
 	var woolfs = 0
-	if(d.fire<d.k21): woolfs = d.k22
+	if (d.fire < d.k21): 
+		woolfs = d.k22
 	
-	d.people = d.people - d.k31*woolfs - d.k41 + d.k42*d.y
-	
+	d.people = d.people - woolfs - d.k41 + d.k42*d.y
+	# d.people = d.people + 0.5*woolfs*d.x	# 
 	if (d.people>100): d.people=100
 	if (d.people<0): d.people=0
 	
 	d.turn += 1
-	
+	$Control/time_label.text = str(d.k02 * d.x * drop)
+
 
 func update_labels():
 	$Control/fire.text = str(int(d.fire))
@@ -89,12 +108,9 @@ func update_labels():
 	
 	$Control/turn.text = str(int(d.turn))
 	#$Control/turn.text = str(int(t))
-	
 
 
 #func _on_update():
-	
-	
 	
 # signals are here:
 
