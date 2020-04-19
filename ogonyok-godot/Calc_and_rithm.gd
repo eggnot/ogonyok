2def7d6c1d3e46b0a178a35ece5e90df8a01c198
jora <el.mexan@gmail.com>
2020-04-19 15:03:59 +0700

    добавлено попадание в ритм

--- ogonyok-godot/Calc_and_rithm.gd
+++ ogonyok-godot/Calc_and_rithm.gd
@@ -1,6 +1,13 @@
 extends Node2D
 
-var t
+var t = 0
+var fase = 0
+var fase_trig = 0.5	# порог срабатывания кнопки
+var k_fase = 100		# длина цикла в кадрах
+var pl_moved = false	# проверка на нажатие кнопки
+
+var auto_fuckup = false
+
 var d = {}
 
 func reset():
@@ -22,7 +29,33 @@ func _ready():
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
+# warning-ignore:unused_argument
+func _process(delta):
+	t+=1		# типа счетчик кадров
+	if t>k_fase: 
+		t=0	# защита от переполнения
+	
+	if auto_fuckup:
+		if t==round(k_fase/2) and !pl_moved:
+			_on_fuckup_pressed()
+	
+	#t = OS.get_time()['second']	# Жестко тормозит
+	fase = cos(2*3.14*t/k_fase)
+	
+	$Control/time_label.text = str(fase)
+	
+	$Control/gather.rect_scale.x = 1+fase/10
+	$Control/gather.rect_scale.y = 1+fase/10
+	
+	$Control/feed.rect_scale.x = 1+fase/10
+	$Control/feed.rect_scale.y = 1+fase/10
+	
+	$Control/dance.rect_scale.x = 1+fase/10
+	$Control/dance.rect_scale.y = 1+fase/10
+	
+	
+#	calc()
+#	update_labels()
 #	pass
 
 func calc():
@@ -48,11 +81,6 @@ func calc():
 	
 	d.turn += 1
 	
-	
-	t+=1
-	
-	print(OS.get_time()['seconds'])
-	
 
 func update_labels():
 	$Control/fire.text = str(int(d.fire))
@@ -60,8 +88,8 @@ func update_labels():
 	$Control/people.text = str(int(d.people))
 	
 	$Control/turn.text = str(int(d.turn))
+	#$Control/turn.text = str(int(t))
 	
-	$Control/time_label.text = str(int(t))
 
 
 #func _on_update():
@@ -71,19 +99,35 @@ func update_labels():
 # signals are here:
 
 func _on_gather_pressed():
-	d.x=0; d.y=0; d.z=1
+	if fase>fase_trig:
+		d.x=0; d.y=0; d.z=1
+		pl_moved = true
+	else:
+		d.x=0; d.y=0; d.z=0
+		pl_moved = false
 	calc()
 	update_labels()
 
 
 func _on_feed_pressed():
-	d.x=1; d.y=0; d.z=0
+	if fase>fase_trig:
+		d.x=1; d.y=0; d.z=0
+		pl_moved = true
+	else:
+		d.x=0; d.y=0; d.z=0
+		pl_moved = false
 	calc()
 	update_labels()
 
 
 func _on_dance_pressed():
-	d.x=0; d.y=1; d.z=0
+	if fase>fase_trig:
+		d.x=0; d.y=1; d.z=0
+		pl_moved = true
+	else:
+		d.x=0; d.y=0; d.z=0
+		pl_moved = false
+
 	calc()
 	update_labels()
 
